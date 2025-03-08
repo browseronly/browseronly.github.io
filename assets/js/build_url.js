26381d7c94a0da1619456364e852566c65a96548
xyz8848 <xyz88488848@126.com>
2025-03-08 18:06:52 +0800

    copy button

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -1,10 +1,24 @@
-function build_url(){
+function build_url() {
     var url = encodeURI(decodeURI(document.querySelector('#url').value));
-    if (url === "" || url.indexOf("http") === -1){
-        document.getElementById("b_url").innerHTML=`Error: 输入的不是链接或者未加http请求头！`;
+    if (url === "" || url.indexOf("http") === -1) {
+        document.getElementById("b_url").innerHTML = `Error: 输入的不是链接或者未加http请求头！`;
+    } else {
+        var generatedUrl = document.location.href + "api/?url=" + url;
+        document.getElementById("b_url").innerHTML = `
+            <a href="${generatedUrl}" target="_blank">${generatedUrl}</a>
+            <button onclick="copyToClipboard('${generatedUrl}')">复制</button>
+        `;
     }
-    else {
-        url = document.location.href + "api/?url=" + url;
-        document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>${url}</a>`;
+}
+
+function copyToClipboard(url) {
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(url).then(function() {
+            alert('URL 已复制到剪贴板！');
+        }, function(err) {
+            alert('复制 URL 失败: ' + err);
+        });
+    } else {
+        alert('您的浏览器不支持复制到剪贴板。');
     }
 }
